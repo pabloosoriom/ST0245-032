9529aea4151c382178185536487a3fbd2b626544
pabloosoriom <41337774+pabloosoriom@users.noreply.github.com>
2018-07-17 16:30:47 -0500

    Add files via upload

--- talleres/taller01/Main.java
+++ talleres/taller01/Main.java
@@ -1,3 +1,10 @@
+
+/**
+ * Main para la ejecución de los porgramas diseñados para el taller
+ * 
+ * @author Pablo A. Osorio Marulanda y Verónica Mendoza Iguarán
+ * @version 17/07/2018
+ */
 public class Main {
 
     public static void main(String[] args) {
@@ -14,15 +21,15 @@ public class Main {
         System.out.printf("Punto: {%f, %f}\n", p.x(), p.y());
         // 1*sqrt(2)
         System.out.println("Radio Polar: " + p.radioPolar());
-        // -45
+        // -45 "(Debería ser 135)"
         System.out.println("Angulo Polar: " + Math.toDegrees(p.anguloPolar()));
         // 2
-        System.out.println("Angulo Polar: " + p.distanciaEuclidiana(new Punto(1, 1)));
+        System.out.println("Distancia euclidiana: " + p.distanciaEuclidiana(new Punto(1, 1)));
 
         System.out.println();
 
-        Fecha f1 = new Fecha(1, 8, 2017);
-        Fecha f2 = new Fecha(2, 5, 2016);
+        Fecha f1 = new Fecha((byte)1,(byte)8, (short)2017);
+        Fecha f2 = new Fecha( (byte)2,(byte)5, (short)2016);
         System.out.println(f1);
         System.out.println(f2);
         // 1
--- talleres/taller01/Punto.java
+++ talleres/taller01/Punto.java
@@ -1,15 +1,16 @@
 
 /**
- * Write a description of class Punto here.
+ * Punto 1 del taller 1 de estructuras de datos y algoritmos
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Pablo A. Osorio Marulanda- Verónica Mendoza Iguarán
+ * @version 17/07/2018
  */
 import java.util.*;
 public class Punto
 {
     private double x, y;
-
+    /** Constructor para atriubutos 
+     */
     public Punto(double x, double y) {
         this.x=x;
         this.y=y;
@@ -22,18 +23,24 @@ public class Punto
     public double y() {
         return this.x;
     }
-    
+    /** 
+     * Distancia desde el origen hasta el punto
+     */
     public double radioPolar() {
         double radio=Math.sqrt(((x*x)+(y*y)));
         return radio;
     }
-    
+    /**
+     * Ángulo formado por la linea polar del origen y el punto
+     */    
     public double anguloPolar() {
         double angulo=Math.atan2(y,x);
         return angulo;
     }
    
-
+    /** 
+     * Distancia entre dos puntos
+     */
     public double distanciaEuclidiana(Punto otro) {
      double distancia= Math.sqrt((Math.pow(otro.x()-x,2)+Math.pow(otro.y()-y,2)));
      return distancia;

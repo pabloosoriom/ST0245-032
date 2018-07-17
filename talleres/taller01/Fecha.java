f87d1eff0db88c995fa2c9ff77344fc6e4023524
pabloosoriom <41337774+pabloosoriom@users.noreply.github.com>
2018-07-17 12:01:19 -0500

    Add files via upload

--- talleres/taller01/Fecha.java
+++ talleres/taller01/Fecha.java
@@ -1,28 +1,77 @@
+
+/**
+ * Write a description of class Fecha here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
 public class Fecha {
-    public Fecha() {
+    private final byte dia;
+    private final byte mes;
+    private final short anio;
 
+    /**
+     * Se inicializan las variables globales en el constructor de manera que no posean valores nulos o 0s.
+     */
+    public Fecha(byte dia, byte mes, short anio) {
+        this.dia=dia;
+        this.mes=mes;
+        this.anio=anio;
     }
 
-    public int dia() {
-
+    /**
+     * Método para obtener la variable global dia.
+     *
+     * @return el dia
+     */
+    public byte dia() {
+        return this.dia;
     }
 
-    public int mes() {
-
+    /**
+     * Método para obtener la variable global mes.
+     *
+     * @return el mes
+     */
+    public byte mes() {
+        return this.mes;
     }
 
-    public int anio() {
-
+    /**
+     * Método para obtener la variable global anio.
+     *
+     * @return el año
+     */
+    public short anio() {
+        return this.anio;
     }
 
-    // -1 si esta fecha es anterior a la otra
-    // 0 si son iguales
-    // 1 si esta fecha es posterior a la otra
+    /**
+     * @param otra representa la fecha con la culase va a comparar.
+     *
+     * El método comprar se encarga de devolvernos respuesta a tres posibilidades.
+     * 1: si la fecha es menor que la otra retorna -1.
+     * 2: si la fecha es igual que la otra retorna 0.
+     * 3: si la fecha es mayor que la otra retorna 1.
+     *
+     * @return -1 sí es menor; 0 sí es igual; 1 sí es mayor.
+     *
+     */
+
     public int comparar(Fecha otra) {
-
+      if(anio<otra.anio()){
+        return -1;
+        }else if(anio==otra.anio()){}
+        return 0;
     }
 
+    /**
+     * toString se encargará de convertir el tipo abstracto fecha en un tipo cadena
+     * para su posterior visualización
+     *
+     * @return una cadena que contiene la fecha
+     */
     public String toString() {
-        
+     return "";
     }
 }
--- talleres/taller01/Punto.java
+++ talleres/taller01/Punto.java
@@ -1,30 +1,41 @@
-public class Punto {
+
+/**
+ * Write a description of class Punto here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.util.*;
+public class Punto
+{
     private double x, y;
 
-    public Punto() {
-
+    public Punto(double x, double y) {
+        this.x=x;
+        this.y=y;
     }
 
     public double x() {
-
+        return this.x;
     }
 
     public double y() {
-
+        return this.x;
     }
-
-    // http://mathworld.wolfram.com/PolarCoordinates.html
+    
     public double radioPolar() {
-
+        double radio=Math.sqrt(((x*x)+(y*y)));
+        return radio;
     }
-
-    // http://mathworld.wolfram.com/PolarCoordinates.html
+    
     public double anguloPolar() {
-
+        double angulo=Math.atan2(y,x);
+        return angulo;
     }
+   
 
-    // http://mathworld.wolfram.com/Distance.html
     public double distanciaEuclidiana(Punto otro) {
-        
+     double distancia= Math.sqrt((Math.pow(otro.x()-x,2)+Math.pow(otro.y()-y,2)));
+     return distancia;
     }
 }
